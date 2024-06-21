6d8a5bc8ade5f58170be6a66527195edce3f2ae4
SACHIN <ghoshsachin2301@gmail.com>
2024-06-21 14:37:50 +0530

    Day 2

---
+++ Operators.java
@@ -0,0 +1,53 @@
+class Operators {
+        public static void main(String[] args) {
+
+            // *  Arithmatic operators * //
+            // int num1 = 7;
+            // int num2 = 3;
+    
+            // int result = num1 + num2;
+            // int result = num1 - num2;
+            // int result = num1 * num2;
+            // int result = num1 % num2;
+
+            // num1 = num1 + 2;
+            // num1++;     //post increment
+            // num1 += 2;
+            // ++num1;      //pre increment
+
+            // int result = ++num1;
+            // int result = num1++;
+
+
+            // * Relational Operators * //
+
+            // int num1 = 7;
+            // int num2 = 3;
+
+            // boolean result = num1 > num2;
+            // boolean result = num1 < num2;
+            // boolean result = num1 <= num2;
+            // boolean result = num1 >= num2;
+            // boolean result = num1 == num2;
+
+
+            // * Logical Operators * //
+
+            int x = 7;
+            int y = 3;
+            int a = 5;
+            int b = 9;
+
+            // boolean result = x > y && a < b;
+            // boolean result = x > y || a < b;
+            // boolean result = x > y && a < b && x > 1;
+            boolean result = x > y || a < b || x > 1;
+
+
+            System.out.println("The result is " + !result);
+            System.out.println("The result is " + result);
+
+            // System.out.println("The result is " + num1);
+        
+    }
+}
