94ec824e1b02344587dfa98568c2be51ef3b9779
SACHIN <ghoshsachin2301@gmail.com>
2024-06-21 12:17:50 +0530

    Day 1

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello
+{
+    public static void main(String arr[])
+    {
+        String num1 = "world";
+        String num2 = "hello";
+        System.out.println(num1 + num2);
+        System.out.println("Hello World");
+    }
+
+}
