100899abfce5d8cc2b07edec3981f665529e82c5
SACHIN <ghoshsachin2301@gmail.com>
2024-06-22 15:24:54 +0530

    Day 3

---
+++ Conditional.java
@@ -0,0 +1,34 @@
+class Conditional {
+    public static void main(String[] args) {
+        // int x = 18;
+        // int y = 23;
+
+        // if(x>y) //for multiple statements use {} 
+        // {
+        //     System.out.println("true");
+        //     System.out.println(x + "is greater than" + y);
+        // }
+        // else
+        // {
+        //     System.out.println("false");
+        //     System.out.println(y + "is greater than" + x);   
+        // }
+
+        int x = 3;
+        int y = 6;
+        int z = 9;
+
+        if(x>y && x>z)
+        {
+            System.out.println("x is the greatest number");
+            System.out.println("True");
+            }
+        else if(y>z)
+        {
+            System.out.println("y is the greatest number");
+            System.out.println("True");
+            }
+        else
+            System.out.println("z is the greatest number");
+ }
+}
